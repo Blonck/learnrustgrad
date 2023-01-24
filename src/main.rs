fa8739633073c0eedfcda383a5ae12d73f25686f
Martin Marenz <martin.marenz@webfleet.com>
2023-01-24 06:59:15 +0100

    Gradient calculation for Add,Mul, and Tanh

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::borrow::Borrow;
 use std::borrow::Cow;
 use std::cell::RefCell;
 use std::fmt;
@@ -8,13 +7,34 @@ use std::ops;
 use ptree::style::Style;
 use ptree::TreeItem;
 
+pub use crate::ops::*;
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum ScalarOp {
+    None,
+    Add,
+    Mul,
+    Tanh,
+}
+
+impl fmt::Display for ScalarOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ScalarOp::None => write!(f, ""),
+            ScalarOp::Add => write!(f, "+"),
+            ScalarOp::Mul => write!(f, "*"),
+            ScalarOp::Tanh => write!(f, "tanh"),
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 struct Scalar<'a> {
     val: f32,
     grad: RefCell<f32>,
     lhs_parent: Option<&'a Scalar<'a>>,
     rhs_parent: Option<&'a Scalar<'a>>,
-    op: String,
+    op: ScalarOp,
 }
 
 impl<'a> Scalar<'a> {
@@ -24,7 +44,7 @@ impl<'a> Scalar<'a> {
             grad: RefCell::new(0.0),
             lhs_parent: None,
             rhs_parent: None,
-            op: "".to_string(),
+            op: ScalarOp::None,
         }
     }
 
@@ -32,7 +52,7 @@ impl<'a> Scalar<'a> {
         val: f32,
         lhs_parent: Option<&'b Scalar>,
         rhs_parent: Option<&'b Scalar>,
-        op: String,
+        op: ScalarOp,
     ) -> Scalar<'b> {
         Scalar {
             val,
@@ -44,26 +64,35 @@ impl<'a> Scalar<'a> {
     }
 
     fn calc_grad(&self) {
-        if self.op == "+" {
-            let lhs = self.lhs_parent.unwrap();
-            let rhs = self.rhs_parent.unwrap();
+        match self.op {
+            ScalarOp::Add => {
+                let lhs = self.lhs_parent.unwrap();
+                let rhs = self.rhs_parent.unwrap();
 
-            let old_lgrad = lhs.grad.borrow().clone();
-            *lhs.grad.borrow_mut() = old_lgrad + *self.grad.borrow();
+                let old_lgrad = lhs.grad.borrow().clone();
+                *lhs.grad.borrow_mut() = old_lgrad + *self.grad.borrow();
 
-            let old_rgrad = rhs.grad.borrow().clone();
-            *rhs.grad.borrow_mut() = old_rgrad + *self.grad.borrow();
-        } else if self.op == "*" {
-            let lhs = self.lhs_parent.unwrap();
-            let rhs = self.rhs_parent.unwrap();
+                let old_rgrad = rhs.grad.borrow().clone();
+                *rhs.grad.borrow_mut() = old_rgrad + *self.grad.borrow();
+            }
+            ScalarOp::Mul => {
+                let lhs = self.lhs_parent.unwrap();
+                let rhs = self.rhs_parent.unwrap();
 
-            let old_lgrad = lhs.grad.borrow().clone();
-            *lhs.grad.borrow_mut() = old_lgrad + *self.grad.borrow() * rhs.val;
+                let old_lgrad = lhs.grad.borrow().clone();
+                *lhs.grad.borrow_mut() = old_lgrad + *self.grad.borrow() * rhs.val;
 
-            let old_rgrad = rhs.grad.borrow().clone();
-            *rhs.grad.borrow_mut() = old_rgrad + *self.grad.borrow() * lhs.val;
-        } else {
-            panic!("Unknown operation");
+                let old_rgrad = rhs.grad.borrow().clone();
+                *rhs.grad.borrow_mut() = old_rgrad + *self.grad.borrow() * lhs.val;
+            }
+            ScalarOp::Tanh => {
+                let lhs = self.lhs_parent.unwrap();
+
+                let old_lgrad = lhs.grad.borrow().clone();
+
+                *lhs.grad.borrow_mut() = old_lgrad + (1.0 - self.val * self.val) * (*self.grad.borrow());
+            }
+            _ => {}
         }
     }
 }
@@ -78,7 +107,7 @@ impl<'a> ops::Add for &'a Scalar<'a> {
     type Output = Scalar<'a>;
 
     fn add(self, rhs: &'a Scalar) -> Self::Output {
-        let out = Scalar::new_with_parents(self.val + rhs.val, Some(self), Some(rhs), "+".to_string());
+        let out = Scalar::new_with_parents(self.val + rhs.val, Some(self), Some(rhs), ScalarOp::Add);
 
         out
     }
@@ -88,7 +117,7 @@ impl<'a> ops::Add<f32> for &'a Scalar<'a> {
     type Output = Scalar<'a>;
 
     fn add(self, rhs: f32) -> Self::Output {
-        Scalar::new_with_parents(self.val + rhs, Some(self), None, "+".to_string())
+        Scalar::new_with_parents(self.val + rhs, Some(self), None, ScalarOp::Add)
     }
 }
 
@@ -96,7 +125,7 @@ impl<'a> ops::Add<&'a Scalar<'a>> for f32 {
     type Output = Scalar<'a>;
 
     fn add(self, rhs: &'a Scalar) -> Self::Output {
-        Scalar::new_with_parents(self + rhs.val, None, Some(rhs), "+".to_string())
+        Scalar::new_with_parents(self + rhs.val, None, Some(rhs), ScalarOp::Add)
     }
 }
 
@@ -104,7 +133,7 @@ impl<'a> ops::Mul for &'a Scalar<'a> {
     type Output = Scalar<'a>;
 
     fn mul(self, rhs: &'a Scalar) -> Self::Output {
-        Scalar::new_with_parents(self.val * rhs.val, Some(self), Some(rhs), "+".to_string())
+        Scalar::new_with_parents(self.val * rhs.val, Some(self), Some(rhs), ScalarOp::Mul)
     }
 }
 
@@ -112,7 +141,7 @@ impl<'a> ops::Mul<f32> for &'a Scalar<'a> {
     type Output = Scalar<'a>;
 
     fn mul(self, rhs: f32) -> Self::Output {
-        Scalar::new_with_parents(self.val * rhs, Some(self), None, "+".to_string())
+        Scalar::new_with_parents(self.val * rhs, Some(self), None, ScalarOp::Mul)
     }
 }
 
@@ -120,19 +149,19 @@ impl<'a> ops::Mul<&'a Scalar<'a>> for f32 {
     type Output = Scalar<'a>;
 
     fn mul(self, rhs: &'a Scalar) -> Self::Output {
-        Scalar::new_with_parents(self * rhs.val, None, Some(rhs), "+".to_string())
+        Scalar::new_with_parents(self * rhs.val, None, Some(rhs), ScalarOp::Mul)
     }
 }
 
 impl<'a> Scalar<'a> {
     fn tanh(&'a self) -> Scalar<'a> {
-        Scalar::new_with_parents(self.val.tanh(), Some(self), None, "tanh()".to_string())
+        Scalar::new_with_parents(self.val.tanh(), Some(self), None, ScalarOp::Tanh)
     }
 }
 
 impl fmt::Display for Scalar<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if self.op != "" {
+        if self.op != ScalarOp::None {
             write!(f, "Val({}; Δ{})<- {}", self.val, self.grad.borrow(), self.op)
         } else {
             write!(f, "Val({}; Δ{})", self.val, self.grad.borrow())
@@ -180,4 +209,35 @@ fn main() {
     let h = g.tanh();
 
     ptree::print_tree(&&h).expect("Print tree error!");
+
+    let x1 = Scalar::new(2.0);
+    let x2 = Scalar::new(0.0);
+    
+    let w1 = Scalar::new(-3.0);
+    let w2 = Scalar::new(1.0);
+
+    let b = Scalar::new(6.8813735870195432);
+
+    let x1w1 = &x1 * &w1;
+    let x2w2 = &x2 * &w2;
+    let x1w1x2w2 = &x1w1 + &x2w2;
+
+    let n = &x1w1x2w2 + &b;
+    let o = n.tanh();
+
+    *o.grad.borrow_mut() = 1.0;
+
+    o.calc_grad();
+    n.calc_grad();
+    x1w1x2w2.calc_grad();
+    x1w1.calc_grad();
+    x2w2.calc_grad();
+    b.calc_grad();
+    w2.calc_grad();
+    w1.calc_grad();
+    x2.calc_grad();
+    x1.calc_grad();
+
+    ptree::print_tree(&&o).expect("Print tree error!");
+
 }
